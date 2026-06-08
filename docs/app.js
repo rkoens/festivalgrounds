d754261eaf3a13c480d69ee175ff1f9deafb2edb
rkoens <84374390+rkoens@users.noreply.github.com>
2026-06-08 10:34:43 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,46 +1,86 @@
 async function loadData() {
 
     const response = await fetch("history.json");
-
     const data = await response.json();
 
     if (!data.length) return;
 
     const latest = data[data.length - 1];
-    
+
+    //
+    // SOLD LAST 24H
+    //
+
     const latestTime = new Date(latest.timestamp);
-    
+
     const cutoff24h = new Date(
         latestTime.getTime() - 24 * 60 * 60 * 1000
     );
-    
+
     let record24hAgo = null;
-    
+
     for (let i = data.length - 1; i >= 0; i--) {
-    
+
         const t = new Date(data[i].timestamp);
-    
+
         if (t <= cutoff24h) {
             record24hAgo = data[i];
             break;
         }
     }
-    
+
     let sold24h = 0;
-    
+
     if (record24hAgo) {
         sold24h =
             latest.sold -
             record24hAgo.sold;
     }
 
-    if (data.length >= 25) {
+    //
+    // SOLD LAST HOUR
+    //
 
-        const yesterday = data[data.length - 25];
+    const cutoff1h = new Date(
+        latestTime.getTime() - 60 * 60 * 1000
+    );
 
-        document.getElementById(
-            "sold24h"
-        ).textContent = sold24h;
+    let record1hAgo = null;
+
+    for (let i = data.length - 1; i >= 0; i--) {
+
+        const t = new Date(data[i].timestamp);
+
+        if (t <= cutoff1h) {
+            record1hAgo = data[i];
+            break;
+        }
+    }
+
+    let sold1h = 0;
+
+    if (record1hAgo) {
+        sold1h =
+            latest.sold -
+            record1hAgo.sold;
+    }
+
+    //
+    // UPDATE DASHBOARD
+    //
+
+    document.getElementById("totalSold").textContent =
+        latest.sold;
+
+    document.getElementById("sold24h").textContent =
+        sold24h;
+
+    document.getElementById("sold1h").textContent =
+        sold1h;
+
+    //
+    // CHART
+    //
 
     const labels = data.map(
         x => new Date(x.timestamp).toLocaleString()
@@ -54,7 +94,6 @@ async function loadData() {
         document.getElementById("salesChart"),
         {
             type: "line",
-
             data: {
                 labels,
                 datasets: [
